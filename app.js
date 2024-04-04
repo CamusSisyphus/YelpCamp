dca32896f64db8055fb91a0ec1a3d9e71382b5fc
Your NameCamusSisyphus <masonwu624@gmail.com>
2024-04-04 00:08:48 -0400

    add Session/Flash/Passport Authentication

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const mongoose = require('mongoose');
 const Campground = require("./models/campground");
 const Review = require("./models/review");
 const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate')
+const ejsMate = require('ejs-mate');
+const flash = require('connect-flash')
+const session = require('express-session');
 const {campgroundSchema, reviewSchema} = require("./schemas")
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
+const usersRoutes = require('./routes/users');
+const User = require('./models/user');
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const db =  mongoose.connection;
@@ -28,11 +32,38 @@ app.set('views',path.join(__dirname,'views'));
 app.engine("ejs",ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 *7,
+        maxAge: Date.now() + 1000 * 60 * 60 * 24 *7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
+app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
-app.use('/campgrounds',campgrounds)
-app.use('/campgrounds/:id/reviews',reviews)
+app.use('/', usersRoutes);
+
+app.use('/campgrounds',campgroundsRoutes)
+app.use('/campgrounds/:id/reviews',reviewsRoutes)
 
 app.get('/', (req,res) =>{
     res.render('home')
