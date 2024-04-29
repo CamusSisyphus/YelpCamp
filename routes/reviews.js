2ba5844459aae9c8df4b038384ad549843ccdbf2
Your NameCamusSisyphus <masonwu624@gmail.com>
2024-04-29 17:52:01 -0400

    add review permission for login only

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router({mergeParams : true});
 const catchAsync = require('../utils/catchAsync');
-const {validateReview}= require('../middleware');
+const {isLoggedIn,validateReview}= require('../middleware');
 
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
@@ -9,7 +9,7 @@ const Review = require("../models/review");
 
 
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
