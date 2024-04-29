d6ccc878c28122f972e78e381b0bf11bfdd46dba
Your NameCamusSisyphus <masonwu624@gmail.com>
2024-04-29 17:44:37 -0400

    added authorization/validation to middleware

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random()*20) + 10;
         const camp = new Campground({ 
+            author:'660dc754d2bdec84013d3a0a',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title : `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
