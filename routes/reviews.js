ec9c69c8eac7cb56625a20139aba0dc5a49dd284
Your NameCamusSisyphus <masonwu624@gmail.com>
2024-04-29 20:54:13 -0400

    update review.author

--- routes/reviews.js
+++ routes/reviews.js
@@ -12,6 +12,7 @@ const Review = require("../models/review");
 router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
